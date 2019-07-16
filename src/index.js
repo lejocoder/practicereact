c2800a7fe0b1df58ceedb385c3b815f8cffb4297
john le <jcle1@uci.edu>
2019-07-15 19:21:55 -0700

    firstpractice

--- src/index.js
+++ src/index.js
@@ -1,12 +1,34 @@
-import React from 'react';
+import React , { useState } from 'react'
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+function App() {
+    const [items, setItems] = useState([])
+    const addItem = () => [
+        setItems([... items, {
+            id: items.length,
+            value: Math.floor(Math.random()* 10) + 1
+        }])
+    ]
+    const modify = () => {
+        let itemsTwo = [...items]
+        itemsTwo[0] = items[0] + 1
+        setItems(itemsTwo)
+    }
+    
+    return (
+        <div>
+            <button onClick = {addItem}> Add a number</button>
+            <button onClick = {modify}> modify </button>
+            <ul>
+                {
+                    items.map(item => (
+                        <li key= {item.id}>{item.value}</li>
+                    ))
+                }
+            </ul>
+        </div>
+    )
+}
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// this is how you work through a normal push. now try to figure how to modify
+//export default App
+ReactDOM.render(<App/>, document.getElementById('root'));
