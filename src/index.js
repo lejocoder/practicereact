38c510df34c1bb66f4b123cbec7bfef9c9ad366e
john le <jcle1@uci.edu>
2019-07-17 14:47:00 -0700

    finished part2b practices

--- src/index.js
+++ src/index.js
@@ -1,34 +1,51 @@
-import React , { useState } from 'react'
-import ReactDOM from 'react-dom';
-function App() {
-    const [items, setItems] = useState([])
-    const addItem = () => [
-        setItems([... items, {
-            id: items.length,
-            value: Math.floor(Math.random()* 10) + 1
-        }])
-    ]
-    const modify = () => {
-        let itemsTwo = [...items]
-        itemsTwo[0] = items[0] + 1
-        setItems(itemsTwo)
-    }
-    
-    return (
-        <div>
-            <button onClick = {addItem}> Add a number</button>
-            <button onClick = {modify}> modify </button>
-            <ul>
-                {
-                    items.map(item => (
-                        <li key= {item.id}>{item.value}</li>
-                    ))
-                }
-            </ul>
-        </div>
-    )
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const notes = [
+  {
+    id: 1,
+    content: 'HTML is easy',
+    date: '2019-05-30T17:30:31.098Z',
+    important: true
+  },
+  {
+    id: 2,
+    content: 'Browser can execute only Javascript',
+    date: '2019-05-30T18:39:34.091Z',
+    important: false
+  },
+  {
+    id: 3,
+    content: 'GET and POST are the most important methods of HTTP protocol',
+    date: '2019-05-30T19:20:14.298Z',
+    important: true
+  }
+]
+
+const Note = ({ note }) => {
+  return (
+    <li>{note.content}</li>
+  )
 }
 
-// this is how you work through a normal push. now try to figure how to modify
-//export default App
-ReactDOM.render(<App/>, document.getElementById('root'));
+const App = ({ notes }) => {
+  const rows = () => notes.map(note =>
+    <Note 
+      key={note.id}
+      note={note}
+    />
+  )
+
+  return (
+    <div>
+      <h1>Notes</h1>
+      <ul>
+        {rows()}
+      </ul>
+    </div>
+  )
+}
+ReactDOM.render(
+  <App notes={notes} />,
+  document.getElementById('root')
+)
